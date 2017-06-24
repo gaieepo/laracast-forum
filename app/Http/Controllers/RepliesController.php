0179ba9ef764d1386c48629b456b8505a00e72e1
gaieepo <gaieepo@gmail.com>
2017-06-24 20:38:28 +0800

    detect spam for other places

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -49,9 +49,12 @@ class RepliesController extends Controller
         return back();
     }
 
-    public function update(Reply $reply)
+    public function update(Reply $reply, Spam $spam)
     {
         $this->authorize('update', $reply);
+
+        $this->validate(request(), ['body' => 'required']);
+        $spam->detect(request('body'));
 
         $reply->update(request(['body']));
     }
