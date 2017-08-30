5d5e27497b61319843a26398720f82ca0361f4c8
gaieepo <gaieepo@gmail.com>
2017-08-31 01:12:29 +0800

    frontend of avatar

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -8,6 +8,16 @@
                 <h1>
                     {{ $profileUser->name }}
                 </h1>
+
+                @can ('update', $profileUser)
+                    <form method="POST" action="{{ route('avatar_path', $profileUser)}}" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                        <input type="file" name="avatar">
+                        <button type="submit" class="btn btn-primary">Add Avatar</button>
+                    </form>
+                @endcan
+
+                <img src="{{ asset($profileUser->avatar()) }}" height="100" width="100">
             </div>
 
             @forelse ($activities as $date => $activity)
--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,4 @@ Route::get('/profiles/{user}/notifications', 'UserNotificationsController@index'
 
 // API
 Route::get('/api/users', 'Api\UsersController@index');
-Route::post('/api/users/{user}/avatar', 'Api\UserAvatarController@store')->middleware('auth');
+Route::post('/api/users/{user}/avatar', 'Api\UserAvatarController@store')->middleware('auth')->name('avatar_path');
