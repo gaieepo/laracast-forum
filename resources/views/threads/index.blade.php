a867b608097ee3152c5f0a228f8a969ee7548aad
gaieepo <gaieepo@gmail.com>
2017-09-02 13:30:10 +0800

    isolate trending component

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -10,6 +10,7 @@
         </div>
 
         <div class="col-md-4">
+            @if(count($trending))
             <div class="panel panel-default">
                 <div class="panel-heading">
                     Trending threads
@@ -27,6 +28,7 @@
                     </ul>
                 </div>
             </div>
+            @endif
         </div>
     </div>
 </div>
